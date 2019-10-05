7ac181d7b6f9c4eb2d7fcaea7b6ea15fd8e23923
Osman Zakir <osmanzakir90@hotmail.com>
2019-10-05 21:23:42 +0500

    completed JS code

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,13 @@
 "use strict";
-const unorderedList = document.getElementById("ul");
-const listItem1 = unorderedList.firstChild();
-const listItemLast = unorderedList.lastChild();
-const listItem2 = listItem1.append("li");
-listItem2.textContent = "2";
-const listItem3 = listItemLast.prepend("li");
-listItem3.textContent = "3";
+function updateList() {
+  const listItem2 = document.createElement("li");
+  listItem2.textContent = "2";
+  const listItem1 = document.getElementById("one");
+  listItem1.insertAdjacentElement("afterend", listItem2);
+  const listItem3 = document.createElement("li");
+  listItem3.textContent = "3";
+  const listItem4 = document.getElementById("two");
+  listItem4.insertAdjacentElement("beforebegin", listItem3);
+}
+
+updateList();
